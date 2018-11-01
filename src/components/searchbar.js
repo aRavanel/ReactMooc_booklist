48e2f0ee5f548a8ff5b65117b8fd06e4b154eaa8
aRavanel <alexis_ravanel@hotmail.fr>
2018-11-01 13:59:12 +0100

    searchbar 1_11_18

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-	return <input />;
-};
+// V1
+// const SearchBar = () => {
+	// return <input />;
+// };
+
+// V2
+
+// NB : class based component
+class SearchBar extends Component{
+	
+	constructor(props){
+		super(props);
+		this.state = {term: ''}; // this.state.term initialise a ''
+	}
+
+	// V2A
+	// render() {
+	// 	return <input  onChange={this.onInputChange} />;
+	// }
+	// onInputChange(event){
+	// 	console.log(event.target.value)
+	// }
+
+	// V2B
+	// render() {
+	// 	return <input  onChange={ event => console.log(event.target.value)} />;
+	// }
+
+	// V2C
+	render() {
+		return (
+			<div>
+				<input  
+				value={this.state.term} // controlled component
+				onChange = { event => this.setState({term: event.target.value }) } />
+				{/* Value of the input : {this.state.term} */}
+			</div>
+		);
+	}
+
+}
+
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/searchbar';
-
+import YTSearch from 'youtube-api-search'
 const API_KEY =  'AIzaSyBe51vQk0tGHEScfqVYP4yiLvHakA6ISw4'
+
+
 
 // create a new compoenent. This componnent should produce some HTML
 // const App = function(){
 	// return <div>Hi!</div>;
 // }
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+	console.log(data)
+});
+
+
+// NB : functionnal component si pas de state
 const App = () => {
 	return(
+		// donne une instance de searchbar
 		<div>
-			<SearchBar />
+			<SearchBar /> 
 		</div>
 	);
-
 }
 
 // Take this compnonent's generated HTML and put it on the page (in the DOM)
